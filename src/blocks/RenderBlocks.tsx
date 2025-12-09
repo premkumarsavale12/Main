724d6c87a896ad9ecfd1dd5cba3c80a7885b81dc
=Premkumar Savale <premsavle112@gmail.com>
2025-12-09 22:20:23 +0530

    Added Main Code

---
+++ src/blocks/RenderBlocks.tsx
@@ -0,0 +1,68 @@
+import React, { Fragment } from 'react'
+
+import type { Page } from '@/payload-types'
+
+import { ArchiveBlock } from '@/blocks/ArchiveBlock/Component'
+import { CallToActionBlock } from '@/blocks/CallToAction/Component'
+import { ContentBlock } from '@/blocks/Content/Component'
+import { FormBlock } from '@/blocks/Form/Component'
+import { MediaBlock } from '@/blocks/MediaBlock/Component'
+import { Right } from '@/blocks/Right/Component'
+import { Conten } from '@/blocks/Conten/Component'
+import { MainBlock } from '@/blocks/Main/Component'
+import { ContactBlock } from '@/blocks/Contact/Component'
+import { OemBlock } from '@/blocks/Oem/Component'
+import { Both } from '@/blocks/Both/Component'
+import { ContactsBlocks } from '@/blocks/ContactsBlocks/Component'
+import { DownComponent } from '@/blocks/Down/Component'
+
+
+const blockComponents = {
+  archive: ArchiveBlock,
+  content: ContentBlock,
+  cta: CallToActionBlock,
+  formBlock: FormBlock,
+  mediaBlock: MediaBlock,
+  right: Right,
+  conten: Conten,
+  main: MainBlock,
+  contact: ContactBlock,
+  oem: OemBlock,
+  both: Both,
+  contactsblocks: ContactsBlocks,
+  down: DownComponent
+}
+
+export const RenderBlocks: React.FC<{
+  blocks: Page['layout'][0][]
+}> = (props) => {
+  const { blocks } = props
+
+  const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0
+
+  if (hasBlocks) {
+    return (
+      <Fragment>
+        {blocks.map((block, index) => {
+          const { blockType } = block
+
+          if (blockType && blockType in blockComponents) {
+            const Block = blockComponents[blockType]
+
+            if (Block) {
+              return (
+                <div className="my-16" key={index}>
+
+                  <Block {...block} disableInnerContainer />
+                </div>
+              )
+            }
+          }
+          return null
+        })}
+      </Fragment>
+    )
+  }
+
+  return null
+}
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx'
+import React from 'react'
+
+interface Props {
+  className?: string
+  loading?: 'lazy' | 'eager'
+  priority?: 'auto' | 'high' | 'low'
+}
+
+export const Logo = (props: Props) => {
+  const { loading: loadingFromProps, priority: priorityFromProps, className } = props
+
+  const loading = loadingFromProps || 'lazy'
+  const priority = priorityFromProps || 'low'
+
+  return (
+
+    
+   <>
+   
+   
+   </>
+
+  )
+}
